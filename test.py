db2241340ef678a2297f3135bcd00f413d94cf03
raphael durand <92552039+raph312003@users.noreply.github.com>
2023-12-18 17:24:54 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,45 @@
+import cv2
+import pandas as pd
+import numpy as np
+from ultralytics import YOLO
+
+
+
+model=YOLO('yolov8s.pt')
+
+
+def RGB(event, x, y, flags, param):
+    if event == cv2.EVENT_MOUSEMOVE :  
+        colorsBGR = [x, y]
+        print(colorsBGR)
+        
+
+cv2.namedWindow('RGB')
+cv2.setMouseCallback('RGB', RGB)
+
+cap=cv2.VideoCapture(0)
+
+my_file = open("coco.txt", "r")
+data = my_file.read()
+class_list = data.split("\n")
+print(class_list)
+count=0
+while True:
+    
+    ret,frame = cap.read()
+    if not ret:
+        break
+    count += 1
+    if count % 3 != 0:
+        continue
+    frame=cv2.resize(frame,(1020,500))
+
+    results=model.predict(frame, show=True)
+    
+           
+    cv2.imshow("RGB", frame)
+    if cv2.waitKey(1)&0xFF==27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
